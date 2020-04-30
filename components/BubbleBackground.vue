d328c05669e8ce64f308fa49c21f596be75ce133
DahisC <dahischeng@gmail.com>
2020-04-30 17:18:12 +0800

    working on bubble background

--- components/BubbleBackground.vue
+++ components/BubbleBackground.vue
@@ -1,19 +1,29 @@
 <template>
   <div>
-    <div class="bubble"></div>
+    <div v-for="(bubble, i) in bubbleArray" :key="i" class="bubble"></div>
+    <!-- <div class="bubble"></div> -->
     <slot></slot>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      bubbleArray: [{ size: 30 }, { size: 30 }, { size: 30 }]
+    }
+  },
   mounted() {
+    let bubbleArray = [{ size: 30 }, { size: 30 }, { size: 30 }]
     let ss = document.styleSheets
+    console.log(ss)
+    console.log(this.$options._scopeId)
+
     //a = ss.map(s => s)
     //console.log(a)
-    console.log(ss)
-    console.log(ss)
-    console.log(Array.from(ss).map(s => Array.from(s.cssRules).map(cr => cr)))
+    // console.log(ss)
+    // console.log(ss)
+    // console.log(Array.from(ss).map(s => Array.from(s.cssRules).map(cr => cr)))
   }
 }
 </script>
@@ -34,36 +44,46 @@ div.bubble {
   border-radius: 50%;
   z-index: 999;
   opacity: 1;
-  animation-name: floating;
+  /* animation-name: floating; */
   animation-duration: 20s;
   animation-iteration-count: infinite;
   animation-timing-function: linear;
   animation-fill-mode: forwards;
   /* background-image: url('../assets/images/bubble-border.png');
   background-repeat: no-repeat; */
-  background-color: rgba(255, 255, 255, 0.25);
+  background-color: rgba(255, 255, 255, 1);
 }
 
 @keyframes floating {
   0% {
-    left: 25px;
+    left: 0px;
     bottom: 0%;
-    opacity: 0;
-  }
-  40% {
-    left: 1px;
-    bottom: 40%;
-    opacity: 1;
-  }
-  80% {
-    left: 25px;
-    bottom: 80%;
-    opacity: 1;
   }
   100% {
-    left: 69px;
+    left: 0px;
     bottom: 90%;
-    opacity: 0;
+  }
+}
+
+@keyframes floating2 {
+  0% {
+    left: 50px;
+    bottom: 0%;
+  }
+  100% {
+    left: 50px;
+    bottom: 90%;
+  }
+}
+
+@keyframes floating3 {
+  0% {
+    left: 50px;
+    bottom: 0%;
+  }
+  100% {
+    left: 50px;
+    bottom: 90%;
   }
 }
 </style>
